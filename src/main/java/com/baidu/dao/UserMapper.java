9cc1215c88d0de09bac92c6d9467a8bccf4edb87
QaYs49101331 <qays4910@outlook.com>
2020-10-04 23:12:29 +0800

    添加UserMapper

---
+++ src/main/java/com/baidu/dao/UserMapper.java
@@ -0,0 +1,12 @@
+package com.baidu.dao;
+
+import com.baidu.pojo.User;
+
+/**
+ * @author shkstart
+ * @create 2020-10-04 23:04
+ */
+public interface UserMapper {
+
+    public void  addUser(User user);
+}
